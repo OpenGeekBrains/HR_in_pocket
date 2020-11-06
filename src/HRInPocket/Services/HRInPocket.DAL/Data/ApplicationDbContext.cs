e9720babf5dbcd2002336369233bd074b216d5f0
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-11-06 08:55:49 +0300

    Датаконтекст

---
+++ src/HRInPocket/Services/HRInPocket.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HRInPocket.DAL.Models.Entities;
+using HRInPocket.DAL.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRInPocket.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Properties
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Speciality> Specialties { get; set; }
+        public DbSet<ActivityCategory> ActivityCategories { get; set; }
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<CompanyManager> CompanyManagers { get; set; }
+        public DbSet<Employer> Employers { get; set; } 
+        public DbSet<SystemManager> SystemManagers { get; set; } 
+        #endregion
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
