d40b166be1277f4dbcf2ef28741f53a043baf166
Хайрбек Мамбетов <mnocard@gmail.com>
2020-11-07 22:38:40 +0500

    Базовая реализация MVVM

---
+++ src/HRInPocket/UI/WPF/HRInPocket.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using HRInPocket.WPF.ViewModels.Core;
+
+namespace HRInPocket.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelCore
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+
+        #region Свойства
+
+
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Главное окно";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
