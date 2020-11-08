2afd9b7667c1fd49323a942405a55cb6d031bf43
Хайрбек Мамбетов <mnocard@gmail.com>
2020-11-08 13:54:02 +0500

    IoC & DI

--- src/HRInPocket/UI/WPF/HRInPocket.WPF/App.xaml.cs
+++ src/HRInPocket/UI/WPF/HRInPocket.WPF/App.xaml.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+
+using HRInPocket.WPF.ViewModels;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HRInPocket.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
+
+        private static IHost _Host;
+        public static IHost Host => _Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _Host = null;
+        }
     }
 }
