bb8f25a7b653ec5f5aa72ee1e210d49fc21e30f1
dmitry <dmitry@dtech-ural.ru>
2020-11-12 19:06:59 +0500

    Затестил парсер

---
+++ src/HRInPocket/Test/AngleSharp-Console-Test/AngleSharp-Console-Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+
+
+namespace AngleSharp_Console_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Use the default configuration for AngleSharp
+            var config = Configuration.Default.WithDefaultLoader();
+
+            var parser = new HtmlParser();
+
+            var document = BrowsingContext.
+                New(config).
+                OpenAsync(
+                Url.Create(
+                    "https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&L_save_area=true&area=113&from=cluster_area&showClusters=false"
+                    )).Result;
+            IEnumerable<IElement> items = document.QuerySelectorAll("a")
+                .Where(item => item.ClassName != null && item.ClassName.Contains("bloko-link HH-LinkModifier"));
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.TextContent);
+            }
+            //Console.WriteLine(document.DocumentElement.OuterHtml);
+
+        }
+    }
+}
