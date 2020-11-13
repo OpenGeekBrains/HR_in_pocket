62c4ab5f23f14c523274c074a4efc36f2833ef53
Tarxos <wolfmanr@mail.ru>
2020-11-13 15:18:09 +0300

    обновление миграции для включение TargetTasks и CoverLeter с фиксом типов

--- src/HRInPocket/Services/HRInPocket.DAL/Migrations/20201113121633_Add_TargetTasks_and_CoverLetter.Designer.cs
+++ src/HRInPocket/Services/HRInPocket.DAL/Migrations/20201113121633_Add_TargetTasks_and_CoverLetter.Designer.cs
@@ -4,14 +4,16 @@ using HRInPocket.DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HRInPocket.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201113121633_Add_TargetTasks_and_CoverLetter")]
+    partial class Add_TargetTasks_and_CoverLetter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
