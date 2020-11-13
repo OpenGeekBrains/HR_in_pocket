db0a05ae963f015332882d99adc4e4eee7caa817
Tarxos <wolfmanr@mail.ru>
2020-11-13 17:17:28 +0300

    ViewModel blank for User Profile

---
+++ src/HRInPocket/UI/WEB/HRInPocket/ViewModels/Account/UserProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using HRInPocket.DAL.Models.Entities;
+
+namespace HRInPocket.ViewModels.Account
+{
+    public class UserProfileViewModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
+        /// TODO: заменить Address на ViewModel
+        public List<Address> Addresses { get; set; }
+        public List<string> Emails { get; set; }
+        public List<string> Phones { get; set; }
+    }
+}
