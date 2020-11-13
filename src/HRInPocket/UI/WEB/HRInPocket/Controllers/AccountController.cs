ff0c923ad1da4e067578716c17c0aecd04b380c6
Tarxos <wolfmanr@mail.ru>
2020-11-13 18:28:30 +0300

    AccountController for integration profile

--- src/HRInPocket/UI/WEB/HRInPocket/Controllers/AccountController.cs
+++ src/HRInPocket/UI/WEB/HRInPocket/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using HRInPocket.DAL.Models;
+using HRInPocket.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HRInPocket.Controllers
 {
     public class AccountController : Controller
     {
         // GET
-        public IActionResult Profile() => View();
+        public IActionResult Profile() => View(new UserProfileViewModel
+        {
+            FirstName = "Somebody",
+            Surname = "Something",
+            Patronymic = "From Somebody",
+            Age = 47,
+            Birthday = DateTime.Now.AddYears(-47),
+            Sex = Sex.Other
+        });
     }
 }
