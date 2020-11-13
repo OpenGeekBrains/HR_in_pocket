e4a6be966457d1fc432537006f2a1a6985fdfa8b
Tarxos <wolfmanr@mail.ru>
2020-11-13 15:58:55 +0300

    Profile entity model blank

---
+++ src/HRInPocket/Services/HRInPocket.DAL/Models/Users/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using HRInPocket.DAL.Models.Base;
+using HRInPocket.DAL.Models.Entities;
+
+namespace HRInPocket.DAL.Models.Users
+{
+    public class Profile : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
+        public ICollection<Address> Addresses { get; set; }
+        public ICollection<string> Emails { get; set; }
+        public ICollection<string> Phones { get; set; }
+    }
+}
