539145acfd7f4675210f312e638b78c9f63bab1e
dmitry <dmitry@dtech-ural.ru>
2020-11-14 21:32:04 +0500

    описал сервис парсера

---
+++ src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Service/ParsehhService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HRInPocket.Parsing.hh.ru.Interfaces;
+using HRInPocket.Parsing.hh.ru.Models.Entites;
+
+namespace HRInPocket.Parsing.hh.ru.Service
+{
+    public class ParsehhService : IParsehhService
+    {
+        public IParsehh GetPasrse()
+        {
+            return new Parsehh();
+        }
+    }
+    public class Parsehh : IParsehh
+    {
+        public event Action<Vacancy> Result;
+        public void Parse(string GetParameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
