fabb8f79cb7018ffd8d8ef88d0dffc0d6e19de16
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-11-14 21:24:40 +0300

    ViewComponent Карточка разовой покупки

--- src/HRInPocket/UI/WEB/HRInPocket/Components/TarifCardViewComponent.cs
+++ src/HRInPocket/UI/WEB/HRInPocket/Components/TarifCardViewComponent.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HRInPocket.DAL.Models.Entities;
+﻿using HRInPocket.DAL.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace HRInPocket.Components
 {
