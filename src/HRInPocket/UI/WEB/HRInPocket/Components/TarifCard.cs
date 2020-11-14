5caea86c2d9bfce72a3da370868dad22dedc6e53
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-11-14 20:03:34 +0300

    ViewComponent карточка тарифа

---
+++ src/HRInPocket/UI/WEB/HRInPocket/Components/TarifCard.cs
@@ -0,0 +1,10 @@
+﻿using HRInPocket.DAL.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRInPocket.Components
+{
+    public class TarifCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Tarif tarif) => View(tarif);
+    }
+}
