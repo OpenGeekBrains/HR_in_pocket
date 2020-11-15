74945595febfd2fae434f5390f48b12c9f5abb5b
dmitry <dmitry@dtech-ural.ru>
2020-11-15 14:14:59 +0500

    Добавил описание полей

--- src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Models/Entites/Vacancy.cs
+++ src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Models/Entites/Vacancy.cs
@@ -33,21 +33,25 @@ namespace HRInPocket.Parsing.hh.ru.Models.Entites
         /// Краткое описание вакансии
         /// </summary>
         public string ShortDescription { get; set; }
-        
+
         /// <summary>
         /// Компенсация (зарплата) от N
+        /// Всегда меньше CompensationUp
+        /// Если стоит 0 - то приставка ОТ, а сумму смотреть в CompensationUp
         /// </summary>
         public ulong CompensationDown { get; set; }
 
         /// <summary>
         /// Компенсация (зарплата) до N
+        /// Всегда больше CompensationDown
+        /// Если стоит 0 - то приставка ДО, а сумму смотреть в CompensationDown
         /// </summary>
         public ulong CompensationUp { get; set; }
 
         /// <summary>
-        /// Код Валюты в ISO 4217
+        /// Валюта
         /// </summary>
-        public ushort CurrencyCode { get; set; }
+        public string CurrencyCode { get; set; }
 
         /// <summary>
         /// Прификс (от,до) для компенсации
