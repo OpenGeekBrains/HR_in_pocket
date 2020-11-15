8318e995f78b310b87d15339c0872ce4668dc226
dmitry <dmitry@dtech-ural.ru>
2020-11-15 11:43:13 +0500

    Переопределил свойства сущности Vacancy: Добавил поле CompensationDown/CompensationUp, код валюты CurrencyCode, два поля описания вакансии, краткое и полное

--- src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Models/Entites/Vacancy.cs
+++ src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Models/Entites/Vacancy.cs
@@ -9,17 +9,54 @@ namespace HRInPocket.Parsing.hh.ru.Models.Entites
 {    
     public class Vacancy
     {
+        /// <summary>
+        /// Название вакансии
+        /// </summary>
         public VacancyName Name { get; set; }
 
+        /// <summary>
+        /// Компания, разместившая вакансию
+        /// </summary>
         public Company Company { get; set; }
-        public IEnumerable<BaseUnit> Meta {get;set;}        
 
+        public IEnumerable<BaseUnit> Meta {get;set;}        
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string VacancyAddress { get; set; }
+        /// <summary>
+        /// Описание Вакансии
+        /// </summary>
         public string Description { get; set; }
 
-        public uint Compensation { get; set; }
+        /// <summary>
+        /// Краткое описание вакансии
+        /// </summary>
+        public string ShortDescription { get; set; }
+        
+        /// <summary>
+        /// Компенсация (зарплата) от N
+        /// </summary>
+        public ulong CompensationDown { get; set; }
 
+        /// <summary>
+        /// Компенсация (зарплата) до N
+        /// </summary>
+        public ulong CompensationUp { get; set; }
+
+        /// <summary>
+        /// Код Валюты в ISO 4217
+        /// </summary>
+        public ushort CurrencyCode { get; set; }
+
+        /// <summary>
+        /// Прификс (от,до) для компенсации
+        /// </summary>
         public string PrefixCompensation { get; set; }
 
+        /// <summary>
+        /// Дата размещения вакансии
+        /// </summary>
         public DateTime Date { get; set; }
 
     }
