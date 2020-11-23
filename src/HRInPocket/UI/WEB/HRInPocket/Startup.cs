1e7cdf477645c349bd42cb58720395cd29fc3695
Tarxos <wolfmanr@mail.ru>
2020-11-23 19:36:28 +0300

    replace DI configuration for DB to DIConfiguration class

--- src/HRInPocket/UI/WEB/HRInPocket/Startup.cs
+++ src/HRInPocket/UI/WEB/HRInPocket/Startup.cs
@@ -21,37 +21,13 @@ namespace HRInPocket
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddTransient<TestDbInitializer>();
+            services.AddDB(Configuration);
 
             services.AddAutoMapper(
                 typeof(AccountsProfile)
                 );
 
-            services.AddIdentity<User, IdentityRole>()
-               .AddEntityFrameworkStores<ApplicationDbContext>()
-               .AddDefaultTokenProviders();
-
-            services.Configure<IdentityOptions>(opt =>
-            {
-#if DEBUG
-                opt.Password.RequiredLength = 3;
-                opt.Password.RequireDigit = false;
-                opt.Password.RequireLowercase = false;
-                opt.Password.RequireUppercase = false;
-                opt.Password.RequireNonAlphanumeric = false;
-                opt.Password.RequiredUniqueChars = 3;
-
-#endif
-                opt.User.RequireUniqueEmail = false;
-                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-
-                opt.Lockout.AllowedForNewUsers = true;
-                opt.Lockout.MaxFailedAccessAttempts = 10;
-                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
-            });
+            services.AddServices(Configuration);
 
             services.AddControllersWithViews();
         }
