04b8c055f52852f976e4c14d8ce5f0fa3d578fbb
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-11-25 17:09:30 +0300

    Удалено свойство Age из Profile Добавлено свойство FirstName в Profile Инициализация свойств дефолтными значениями Миграция db

---
+++ src/HRInPocket/Services/HRInPocket.DAL/Migrations/20201125140533_Added_FirstName_and_Removed_Age_in_Profile.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HRInPocket.DAL.Migrations
+{
+    public partial class Added_FirstName_and_Removed_Age_in_Profile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Profiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Profiles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Profiles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
