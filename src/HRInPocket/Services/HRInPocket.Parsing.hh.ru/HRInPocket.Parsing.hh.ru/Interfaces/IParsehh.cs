879d3fe08549dfd8ef3f52f0e7b3e76000a2eded
Хайрбек Мамбетов <mnocard@gmail.com>
2020-11-26 22:59:07 +0500

    Исправлены комментарии интерфейсов парсера

--- src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Interfaces/IParsehh.cs
+++ src/HRInPocket/Services/HRInPocket.Parsing.hh.ru/HRInPocket.Parsing.hh.ru/Interfaces/IParsehh.cs
@@ -8,28 +8,28 @@ using HRInPocket.Parsing.hh.ru.Service;
 
 namespace HRInPocket.Parsing.hh.ru.Interfaces
 {
-    /// <summary> Парсер загружает страницу по адресу https://hh.ru/search/vacancy </summary>
+    /// <summary> Парсер обрабатывает веб-страницы на получение содержащихся на ней вакансий </summary>
     public interface IParsehh
     {
         /// <summary> Событие передающее полученную вакансию </summary>
         event EventHandler<VacancyEventArgs> SendVacancy;
 
-        /// <summary> Парсит страницу hh.ru и возвращает вакансию через событие VacancyEventArgs </summary>
+        /// <summary> Парсит страницу и возвращает вакансию через событие VacancyEventArgs </summary>
         /// <param name="token"> Токен остановки парсера </param>
         /// <param name="page"> Страницу, которую необходимо парсить </param>
         /// <param name="GetParameters"> Ключевые слова для поиска конкретных вакансий</param>
         /// <returns></returns>
         Task ParseAsync(CancellationToken token, string page, string GetParameters);
 
-        /// <summary> Парсит определенную страницу hh.ru на наличие вакансий </summary>
+        /// <summary> Парсит определенную страницу на наличие вакансий </summary>
         /// <param name="token">Токен отмены парсинга</param>
         /// <param name="page"></param>
         /// <returns></returns>
         Task<(Vacancy[] Vacancies, string NextPage)> ParseAsync(CancellationToken token, string page);
 
-        /// <summary> Парсит страницу hh.ru и возвращает вакансию в IAsyncEnumerable </summary>
+        /// <summary> Парсит страницу и возвращает вакансию в IAsyncEnumerable </summary>
         /// <param name="token"> Токен остановки парсера </param>
-        /// <param name="page"> Страницу, которую необходимо парсить </param>
+        /// <param name="page"> Страница, которую необходимо парсить </param>
         /// <param name="GetParameters"> Ключевые слова для поиска конкретных вакансий</param>
         /// <returns> Вакансии в IAsyncEnumerable </returns>
         IAsyncEnumerable<Vacancy> ParseEnumerableAsync(CancellationToken token, string page, string GetParameters);
