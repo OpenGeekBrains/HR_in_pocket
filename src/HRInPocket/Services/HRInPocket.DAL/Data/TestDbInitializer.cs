38ffda35c435b0589e6f70cc60045f15a8a20b8b
Tarxos <ftarxos@gmail.com>
2020-12-07 21:44:50 +0300

    replace IDbInitializer in to interfaces

--- src/HRInPocket/Services/HRInPocket.DAL/Data/TestDbInitializer.cs
+++ src/HRInPocket/Services/HRInPocket.DAL/Data/TestDbInitializer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+
+using HRInPocket.Interfaces.EF;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -55,49 +55,6 @@ namespace HRInPocket.DAL.Data
             {
                 _logger.LogError(e.Message, "Ошибка инициализации БД");
                 throw;
-            }
-        }
-    }
-
-    public interface IDbInitializer
-    {
-        void Initialize();
-        
-        readonly struct Initializer
-        {
-            public readonly DbContext Context;
-            private readonly ILogger<IDbInitializer> _logger;
-
-            private Initializer(DbContext context, ILogger<IDbInitializer> logger)
-            {
-                Context = context;
-                _logger = logger;
-            }
-
-            public static Initializer StartInit(DbContext context, ILogger<IDbInitializer> logger) => new Initializer(context, logger);
-
-            /// <summary>
-            /// Инициализирует таблицу типа данными из перечисления, если они есть и таблица не была до этого инициализирована
-            /// </summary>
-            /// <typeparam name="TEntity">Тип наследованный от 'BaseEntity'</typeparam>
-            /// <param name="data">Данные для инициализации таблицы</param>
-            /// <returns>Контекст базы данных с которым проводилась работа</returns>
-            public Initializer InitTable<TEntity>(IEnumerable<TEntity> data) where TEntity : class
-            {
-                _ = data ?? throw new ArgumentNullException(nameof(data));
-                var entities = data as TEntity[] ?? data.ToArray();
-                if (entities.Length == 0) throw new ArgumentException("There no data to import", nameof(data));
-
-
-                var table = Context.Set<TEntity>();
-                if (table.Any()) return this;
-
-                using var transaction = Context.Database.BeginTransaction();
-                table.AddRange(entities);
-                Context.SaveChanges();
-                transaction.Commit();
-
-                return this;
             }
         }
     }
