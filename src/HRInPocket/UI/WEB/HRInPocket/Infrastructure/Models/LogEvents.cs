9d4ddb2405b825814f5fff5fbba382e8194ecc93
Tarxos <ftarxos@gmail.com>
2020-12-08 19:02:02 +0300

    LogEvents id's

---
+++ src/HRInPocket/UI/WEB/HRInPocket/Infrastructure/Models/LogEvents.cs
@@ -0,0 +1,21 @@
+﻿namespace HRInPocket.Infrastructure.Models
+{
+    public class LogEvents
+    {
+        public const int GenerateItems = 1000;
+        public const int ListItems     = 1001;
+        public const int GetItem       = 1002;
+        public const int InsertItem    = 1003;
+        public const int UpdateItem    = 1004;
+        public const int DeleteItem    = 1005;
+
+        public const int TestItem      = 3000;
+
+        public const int GetItemNotFound    = 4000;
+        public const int UpdateItemNotFound = 4001;
+
+        public const int AccountRegistration = 5000;
+        public const int AccountLogin = 5001;
+        public const int AccountLogout = 5002;
+    }
+}
