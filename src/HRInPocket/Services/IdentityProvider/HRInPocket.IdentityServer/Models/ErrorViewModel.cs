75c120e8f1ee737a692a6e1a2bbacddef3a0f896
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-12-14 22:56:25 +0300

    Добавлен новый MVC проект для IdentityServer

---
+++ src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRInPocket.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
