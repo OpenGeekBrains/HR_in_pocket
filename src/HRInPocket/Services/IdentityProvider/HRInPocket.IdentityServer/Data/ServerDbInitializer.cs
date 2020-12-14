ab757fd2f3a2944698bb6ae8b262f7e55cf1ddcc
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-12-15 01:44:25 +0300

    Инициализатор Бд  сервера

---
+++ src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Data/ServerDbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRInPocket.IdentityServer.InMemoryConfig;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRInPocket.IdentityServer.Data
+{
+    public static class ServerDbInitializer
+    {
+        public static void Init(IServiceProvider ScopeServiceProvider)
+        {
+            ScopeServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = ScopeServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach (var client in DefaultConfig.GetClients())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in DefaultConfig.GetIdentityResources())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in DefaultConfig.GetApiResources())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Program.cs
+++ src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Program.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HRInPocket.IdentityServer.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HRInPocket.IdentityServer
 {
     public class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                ServerDbInitializer.Init(scope.ServiceProvider);
+            }
+            host.Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
