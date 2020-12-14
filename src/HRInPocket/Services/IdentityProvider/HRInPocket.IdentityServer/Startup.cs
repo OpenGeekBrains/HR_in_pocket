60d06535ebec9874251a08358476cc118f33afaa
Антон Алиев <Mr.Aliev.Anton@gmail.com>
2020-12-15 00:35:50 +0300

    Класс Identity-пользователей, датаконтекс, миграция БД

--- src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Startup.cs
+++ src/HRInPocket/Services/IdentityProvider/HRInPocket.IdentityServer/Startup.cs
@@ -1,6 +1,9 @@
+using HRInPocket.IdentityServer.Data;
 using HRInPocket.IdentityServer.InMemoryConfig;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,15 +11,22 @@ namespace HRInPocket.IdentityServer
 {
     public class Startup
     {
+        private readonly IConfiguration _Configuration;
+
+        public Startup(IConfiguration configuration) => _Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UsersDbContext>(config => 
+                config.UseSqlServer(_Configuration.GetConnectionString("UsersDbConnectionString")));
+
             #region InMemoryConfig
             services.AddIdentityServer() // добавляем в систему IdentityServer
                .AddInMemoryIdentityResources(DefaultConfig.GetIdentityResources()) // настройки доступных ресурсов пользователей
                .AddTestUsers(DefaultConfig.GetUsers()) // настройки пользователей
                .AddInMemoryClients(DefaultConfig.GetClients()) // настройки подключаемых клиентов
-               //.AddInMemoryApiResources(DefaultConfig.GetApiResources()) // настройки API ресурсов
-               //.AddInMemoryApiScopes(DefaultConfig.GetApiScopes()) // настройки скопов (то, доступ к чему будет контролироваться)
+               .AddInMemoryApiResources(DefaultConfig.GetApiResources()) // настройки API ресурсов
+               .AddInMemoryApiScopes(DefaultConfig.GetApiScopes()) // настройки скопов (то, доступ к чему будет контролироваться)
                .AddDeveloperSigningCredential(); // только на время разработки. При развертывании необходим реальный сертификат
             #endregion
         }
