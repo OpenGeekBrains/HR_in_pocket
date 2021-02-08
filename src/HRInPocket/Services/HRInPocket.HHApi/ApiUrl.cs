9026e98eb507632d4c84544119a596464db103e6
M95N1K <mysnik999@gmail.com>
2021-02-08 15:37:24 +0600

    Фикс косяков

--- src/HRInPocket/Services/HRInPocket.HHApi/ApiUrl.cs
+++ src/HRInPocket/Services/HRInPocket.HHApi/ApiUrl.cs
@@ -14,6 +14,54 @@
         };
 
         /// <summary>
+        /// Подсказки при поиске и заполнении полей
+        /// </summary>
+        internal class Suggests
+        {
+            private const string suggests = "/suggests";
+
+            /// <summary>
+            /// Подсказки по названиям университетов
+            /// </summary>
+            internal const string educational = suggests + "/educational_institutions";
+
+            /// <summary>
+            /// Подсказки по организациям
+            /// </summary>
+            internal const string companies = suggests + "/companies";
+
+            /// <summary>
+            /// Подсказки по специализациям
+            /// </summary>
+            internal const string specialization = suggests + "/fields_of_study";
+
+            /// <summary>
+            /// Подсказки по ключевым навыкам
+            /// </summary>
+            internal const string skills = suggests + "/skill_set";
+
+            /// <summary>
+            /// Подсказки по должностям
+            /// </summary>
+            internal const string positions = suggests + "/positions";
+
+            /// <summary>
+            /// Подсказка по всем регионам
+            /// </summary>
+            internal const string areas = suggests + "/areas";
+
+            /// <summary>
+            /// Подсказка по всем регионам, являющимися листами в дереве регионов.
+            /// </summary>
+            internal const string areasLeaves = suggests + "/area_leaves";
+
+            /// <summary>
+            /// Подсказки по ключевым словам поиска вакансий
+            /// </summary>
+            internal const string vacancy = suggests + "/vacancy_search_keyword";
+        }
+
+        /// <summary>
         /// Путь к Api поиска компаний
         /// </summary>
         internal const string employers = "/employers";
